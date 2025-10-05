c5e7389974ea0e29b8b0e477450898d85b5f5869
Killy Jack <zicai@openatom.club>
2025-10-06 01:48:02 +0800

    Update log level brush converter for theme support

--- EVBHelper/EVBHelper/Converters/LogLevelToBrushConverter.cs
+++ EVBHelper/EVBHelper/Converters/LogLevelToBrushConverter.cs
@@ -1,8 +1,10 @@
-using System;
-using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
 using Avalonia.Media;
+using Avalonia.Styling;
 using EVBHelper.Services;
+using System;
+using System.Globalization;
 
 namespace EVBHelper.Converters
 {
@@ -15,13 +17,28 @@ namespace EVBHelper.Converters
                 return Brushes.Black;
             }
 
-            return level switch
+            var theme = Application.Current?.ActualThemeVariant;
+
+            if (theme == ThemeVariant.Dark)
             {
-                RfelLogLevel.Info => Brushes.Black,
-                RfelLogLevel.Warning => Brushes.DarkGoldenrod,
-                RfelLogLevel.Error => Brushes.DarkRed,
-                _ => Brushes.Black
-            };
+                return level switch
+                {
+                    RfelLogLevel.Info => Brushes.White,
+                    RfelLogLevel.Warning => Brushes.Yellow,
+                    RfelLogLevel.Error => Brushes.Red,
+                    _ => Brushes.White
+                };
+            }
+            else
+            {
+                return level switch
+                {
+                    RfelLogLevel.Info => Brushes.Black,
+                    RfelLogLevel.Warning => Brushes.DarkGoldenrod,
+                    RfelLogLevel.Error => Brushes.DarkRed,
+                    _ => Brushes.Black
+                };
+            }
         }
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
